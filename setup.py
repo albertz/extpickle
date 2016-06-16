9934716bdb3d345b3d158c6f9db116bc885319cb
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2016-06-16 21:46:06 +0100

    tabs

--- setup.py
+++ setup.py
@@ -11,29 +11,29 @@ import time
 
 
 setup(
-	name = 'extpickle',
-	version = time.strftime("1.%Y%m%d.%H%M%S", time.gmtime()),
-	packages = ['extpickle'],
-	package_dir = {'extpickle': ''},
-	description = 'extended pickle',
-	author = 'Albert Zeyer',
-	author_email = 'albzey@gmail.com',
-	url = 'https://github.com/albertz/extpickle',
-	license = '2-clause BSD license',
-	long_description = open('README.rst').read(),
-	# https://pypi.python.org/pypi?%3Aaction=list_classifiers
-	classifiers = [
-		'Development Status :: 5 - Production/Stable',
-		'Intended Audience :: Developers',
-		'Intended Audience :: Education',
-		'Intended Audience :: Science/Research',
-		'License :: OSI Approved :: BSD License',
-		'Operating System :: MacOS :: MacOS X',
-		'Operating System :: Microsoft :: Windows',
-		'Operating System :: POSIX',
-		'Operating System :: Unix',
-		'Programming Language :: Python',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		]
+    name = 'extpickle',
+    version = time.strftime("1.%Y%m%d.%H%M%S", time.gmtime()),
+    packages = ['extpickle'],
+    package_dir = {'extpickle': ''},
+    description = 'extended pickle',
+    author = 'Albert Zeyer',
+    author_email = 'albzey@gmail.com',
+    url = 'https://github.com/albertz/extpickle',
+    license = '2-clause BSD license',
+    long_description = open('README.rst').read(),
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
 )
 
